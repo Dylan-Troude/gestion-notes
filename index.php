4608bd4c9ed81570ba313220841777c7fbc8ea9a
Tdylan4 <dylan.troude@laplateforme.io>
2025-09-29 14:15:19 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/notes-app/controllers/noteController.php';
+
+include __DIR__ . '/notes-app/views/header.php';
+include __DIR__ . '/notes-app/views/form.php';
+include __DIR__ . '/notes-app/views/notes.php';
+include __DIR__ . '/notes-app/views/footer.php';
