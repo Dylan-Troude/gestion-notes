9bab6f051861695fab8e515e0b7ec966ccd0047c
Tdylan4 <dylan.troude@laplateforme.io>
2025-09-29 15:03:28 +0200

    add: screenshot readme

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/notes-app/controllers/noteController.php';
-
-include __DIR__ . '/notes-app/views/header.php';
-include __DIR__ . '/notes-app/views/form.php';
-include __DIR__ . '/notes-app/views/notes.php';
-include __DIR__ . '/notes-app/views/footer.php';
